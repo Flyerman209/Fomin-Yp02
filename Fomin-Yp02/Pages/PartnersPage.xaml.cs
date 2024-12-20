ee3714cefd62572a9b8d3f2a7fe141daeb1667b6
ROBOT <Duvap666@yandex.ru>
2024-12-20 19:31:42 +0300

    Добавьте файлы проекта.

---
+++ Fomin-Yp02/Pages/PartnersPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fomin_Yp02.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PartnersPage.xaml
+    /// </summary>
+    public partial class PartnersPage : Page
+    {
+        public PartnersPage()
+        {
+            InitializeComponent();
+            var context = Entities.GetContext();
+            var currentPartners = context.Partners.ToList();
+            ListPartners.ItemsSource = currentPartners;
+            UpdatePartners();
+
+        }
+
+        private void UpdatePartners()
+        {
+            //загружаем всех пользователей в список
+            var currentUsers = Entities.GetContext().Partners.ToList();
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddPage(null, true));
+        }
+
+
+        private void Realise_Click(object sender, RoutedEventArgs e)
+        {
+
+            var selectedItem = ListPartners.SelectedItem as Partners;
+            if (selectedItem != null)
+            {
+                NavigationService.Navigate(new Product(selectedItem));
+
+            }
+            else
+            {
+                MessageBox.Show("Выберите партнера");
+            }
+
+        }
+
+        private void Redact_Click(object sender, RoutedEventArgs e)
+        {
+            Partners partners = ListPartners.SelectedItem as Partners;
+            NavigationService?.Navigate(new AddPage(partners, false));
+        }
+    }
+}
